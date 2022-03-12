e76840baaa05b47d2a862d360ba22bc613d5a58c
Tanmay Tupe <tanmaytupe@gmail.com>
2022-03-12 19:57:50 +0530

    Welcome message is now dynamic

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,17 +4,29 @@ import CaptureMandatoryUserDetailsModal from "./Users/CaptureMandatoryUserDetail
 
 const Welcome = () => {
   const userDetails = useSelector((state) => state.userDetails);
+  const userFinSituation = useSelector(
+    (state) => state.presentFinancialSituation
+  );
 
-  console.log('userDetails', userDetails);
+  const getWelcomeMessage = () => {
+    if (userFinSituation){
+      return "Looks like you are enjoying. Thats nice to see!"
+    } else {
+      return "Welcome to your financial simulator. Feel free to explore different \
+      events and simulate your future to take better decisions. Lets start \
+      by capturing some basic information"
+    }
+  
+  }
 
+  console.log("userDetails", userDetails);
+  
   if (userDetails) {
     console.log("user details are available");
     return (
       <Fragment>
         <p>
-          Welcome to your financial simulator. Feel free to explore different
-          events and simulate your future to take better decisions. Lets start
-          by capturing some basic information
+          {getWelcomeMessage()}
         </p>
       </Fragment>
     );
